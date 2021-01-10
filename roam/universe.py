e0fcda48d27dd3d9575e35ede9df5d1eca8a3c01
sapporojones <sapporojones@gmail.com>
2021-01-10 14:00:59 -0800

    initial commit

---
+++ roam/universe.py
@@ -0,0 +1,16 @@
+# for the time being this file contains THE UNIVERSE
+# eventually we will use SDE for system names and 
+# some fancy algorithm for routing and stuff.
+
+# for now we are just gonna define a small batch of systems
+# that we can add on to as needed or subtract from if needed
+# and we'll use eve system names so we can pretend without too much
+# mental exhaustion
+
+#this is a really dumb way to do this
+#TODO: unfuck this
+def universe_list_create():
+    unilist = ['D-PNP9', 'G-YZUX','A1-AUH', 'Q0OH-V', 'X-7BIX', 'C9N-CC']
+    return unilist
+
+
