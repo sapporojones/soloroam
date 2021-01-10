2e900a7af14bcf80cbc56a2aea233200d1f6e219
sapporojones <sapporojones@gmail.com>
2021-01-10 15:44:01 -0800

    I made mistakes...   Removing cache stuff and sqlite SDEs from git

--- soloroam.py
+++ soloroam.py
@@ -5,14 +5,19 @@ by Sapporo Jones
 
 #library mports
 from time import sleep
+import sqlite3
+import requests
+
 
 #local imports
 from roam.ships import Atron
 import roam.words as w
 import roam.system as system
-
+import json
 #TODO: Get SDE and create connection strings to use it here
-
+database = r"sqlite-latest.sqlite"
+conn = sqlite3.connect(database)
+cur = conn.cursor()
 
 
 
@@ -20,14 +25,21 @@ import roam.system as system
 
 unilist = ['D-PNP9', 'G-YZUX','A1-AUH', 'Q0OH-V', 'X-7BIX', 'C9N-CC']
 
+#eventually we'll do argument based routing or some sort of input but for now I am a living god
+
+
+
+
+
 def main(player):
     w.starting_text()
     print(player.location)
     sleep(5)
     system.clear_screen()
     system.current_status(player)
-
+    
 if __name__ == "__main__":
-    player = Atron("player", 100, 100, 100, unilist[0])
+    route_list = system.route_control()
+    player = Atron("player", 100, 100, 100, route_list[0])
 
     main(player)
--- tests/test_roam.py
+++ tests/test_roam.py
@@ -1,5 +1,10 @@
 from roam import __version__
 from roam.ships import Atron
+import roam.system as system
+import sqlite3
+import requests
+import json
+
 
 unilist = ['D-PNP9', 'G-YZUX','A1-AUH', 'Q0OH-V', 'X-7BIX', 'C9N-CC']
 
@@ -26,3 +31,24 @@ def test_player_location_change():
     player.change_location(unilist[1])
     assert player.location == 'G-YZUX'
 
+#db system name lookups testing
+def test_id_by_name():
+    DP = system.find_id_from_system('D-PNP9')
+    assert DP[0] == 30003135
+
+def test_name_by_id():
+    DP = system.find_system_from_id(30003135)
+    assert DP[0] == 'D-PNP9'
+
+def test_route_list_creation():
+    route_list = []
+    origin = 'D-PNP9'
+    desti = 'C9N-CC'
+    ori_id = system.find_id_from_system(origin)[0]
+    dst_id = system.find_id_from_system(desti)[0]
+    formed_route_url = f"https://esi.evetech.net/latest/route/{ori_id}/{dst_id}/?datasource=tranquility&flag=shortest"
+    rsp_list = requests.get(formed_route_url)
+    rsp_list_json = rsp_list.json()
+    for x in rsp_list_json:
+        route_list.append(system.find_system_from_id(x)[0])
+    assert route_list[0] == 'D-PNP9'
